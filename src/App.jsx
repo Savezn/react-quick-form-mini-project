d9c4279e836cf9d9e998f387c62d37de20f92b99
Nut.Thakit <natthakitzn@gmail.com>
2025-03-21 12:00:51 +0700

    feat(formPage): setup useState, onChange event, and basic UI styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import { FormPage } from "./components/FormPage";
+import { SummaryPage } from "./components/SummaryPage";
+
+const bodyStyle = "flex flex-col justify-center items-center h-screen bg-gray-200";
 
 function App() {
   return (
-    <>
+    <div className={bodyStyle}>
       <FormPage />
       <SummaryPage />
-    </>
+    </div>
   );
 }
 
--- src/components/FormPage.jsx
+++ src/components/FormPage.jsx
@@ -1,5 +1,93 @@
 import { useState } from "react";
+import SelectedMovie from "./SelectedMovie";
+
+const formPage = "flex flex-col w-min-content w-1/2 rounded-b-5xl bg-white shadow-xl";
+const movieSurveyHeader = "text-3xl font-bold bg-purple-600 text-white w-full p-6";
+const formContainer = "flex flex-col p-6 bg-white";
+const inputContainer = "flex flex-col gap-2 mb-6";
+const primaryInputBox = "border border-gray-300 rounded-md p-2";
+const textareaInputBox =
+  "resize-y border border-gray-300 rounded-md p-2 w-full";
+const resetButton =
+  "bg-white border border-gray-300 hover:bg-gray-100 text-black py-2 px-4 rounded";
+const submitButton =
+  "bg-purple-500 text-white py-2 px-4 rounded";
+  const buttonContainer = "flex justify-between border-t border-gray-300 pt-6";
 
 export function FormPage() {
-  return <></>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [comment, setComment] = useState("");
+
+  const updateName = (e) => setName(e.target.value);
+  const updateEmail = (e) => setEmail(e.target.value);
+  const updateComment = (e) => setComment(e.target.value);
+  const handleReset = (e) => {};
+  const handleSubmit = (e) => {};
+
+  return (
+    <div className={formPage}>
+      <h1 className={movieSurveyHeader}>Movie Survey</h1>
+      <form className={formContainer}>
+        <div className={inputContainer}>
+          <label htmlFor="name">
+            ชื่อ <span className="text-red-600">*</span>
+          </label>
+
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="กรุณากรอกชื่อของคุณ"
+            className={primaryInputBox}
+            value={name}
+            onChange={updateName}
+          />
+        </div>
+        <div className={inputContainer}>
+          <label htmlFor="email">
+            อีเมล <span className="text-red-600">*</span>
+          </label>
+
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="example@email.com"
+            className={primaryInputBox}
+            value={email}
+            onChange={updateEmail}
+          />
+        </div>
+        <div className={inputContainer}>
+          <SelectedMovie />
+        </div>
+        <div className={inputContainer}>
+          <label>ความคิดเห็นเกี่ยวกับหนัง</label>
+          <textarea
+            id="comment"
+            name="comment"
+            placeholder="พิมพ์ความคิดเห็นของคุณที่นี่..."
+            rows="4"
+            cols="50"
+            className={textareaInputBox}
+            value={comment}
+            onChange={updateComment}
+          />
+        </div>
+        <div className={buttonContainer}>
+          <div>
+            <button type="reset" className={resetButton}>
+              รีเซ็ต
+            </button>
+          </div>
+          <div>
+            <button type="submit" className={submitButton}>
+              ส่งแบบสำรวจ
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
